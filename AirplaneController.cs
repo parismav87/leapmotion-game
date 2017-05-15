a3058ea307f36a640c06dbb6fa6d157ec90c08ea
Paris Mavromoustakos <parismav87@gmail.com>
2017-05-15 13:37:04 +0300

    1st commit
    1st commit

---
+++ AirplaneController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class AirplaneController : MonoBehaviour {
+
+	public HandController handCtrl;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frame frame = handCtrl.GetFrame();
+		Hand hand = frame.Hands.Frontmost;
+		if(hand.IsValid) {
+//			print (hand.PalmPosition.ToUnityScaled());
+			transform.position = new Vector3(hand.PalmPosition.x, hand.PalmPosition.y, 0);
+			transform.rotation = 
+				handCtrl.transform.rotation * hand.Basis.Rotation(false);
+		}
+
+	}
+}
+

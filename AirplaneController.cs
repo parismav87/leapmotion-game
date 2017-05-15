d7d27fef536f778de11215314bdc908119c3a9c2
Paris Mavromoustakos <parismav87@gmail.com>
2017-05-15 14:29:12 +0300

    plane constant speed

--- AirplaneController.cs
+++ AirplaneController.cs
@@ -7,21 +7,22 @@ public class AirplaneController : MonoBehaviour {
 
 	public HandController handCtrl;
 	// Use this for initialization
+	private Vector3 constantVelocity = new Vector3(0,0,100);
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update (){
+		
 		Frame frame = handCtrl.GetFrame();
 		Hand hand = frame.Hands.Frontmost;
 		if(hand.IsValid) {
-//			print (hand.PalmPosition.ToUnityScaled());
-			transform.position = new Vector3(hand.PalmPosition.x, hand.PalmPosition.y, 0);
+			transform.position = new Vector3(hand.PalmPosition.x, hand.PalmPosition.y, transform.position.z); //z should be 0
+			transform.position += constantVelocity * Time.deltaTime;
 			transform.rotation = 
 				handCtrl.transform.rotation * hand.Basis.Rotation(false);
 		}
-
 	}
 }
 
---
+++ CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z-30);
+	}
+
+}
